80345dc311118e54712a9c02da4aeaf9e7f72d33
Aras Ergus <alpha.ergus@gmail.com>
2014-07-25 17:22:10 +0200

    simple parser for game maps

--- Game/Types.hs
+++ Game/Types.hs
@@ -4,11 +4,24 @@ module Types where
 
 import Dummy
 
-newtype GameMap = GM [[Element]]
+newtype GameMap a = GM [[a]]
+
+instance Show a => Show (GameMap a) where
+    show (GM tss) = foldr (\ ts -> ((showLikeString ts ++ "\n") ++)) "" tss
+        where showLikeString = foldr (\ t -> ((show t) ++)) ""
 
 data Element =
     Empty | Wall | Pill | PowerPill | Fruit Flavour |
     Player LambdaMan | Monster Ghost
+
+instance Show Element where
+    show Empty       = " "
+    show Wall        = "#"
+    show Pill        = "."
+    show PowerPill   = "o"
+    show (Fruit _)   = "%"
+    show (Player _)  = "\\"
+    show (Monster _) = "="
 
 data Flavour =
     Cherry | Strawberry | Peach | Apple | Grapes | Galaxian | Bell | Key
@@ -31,9 +44,9 @@ data LambdaMan = LM {
 
 data Position        = Pos { x :: Integer, y :: Integer }
 
-data GameState = GS {
+data GameState a = GS {
     ticks      :: Integer,
-    map        :: GameMap,
+    map        :: GameMap a,
     players    :: [LambdaMan],
     monsters   :: [Ghost],
     frightMode :: Bool
