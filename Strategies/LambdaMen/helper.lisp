ede958e6cbc775a524e25653ecb2779567d233be
Aras Ergus <alpha.ergus@gmail.com>
2014-07-26 21:40:12 +0200

    type signatures & corrections @ LambdaLisp helpers

--- Strategies/LambdaMen/helper.lisp
+++ Strategies/LambdaMen/helper.lisp
@@ -1,31 +1,42 @@
+; accList :: [a] -> Int -> a
 _accList =
 (\r accList x l -> (IF (== l 0) (CAR x) (accList (CDR x) (- l 1))))
 
+; _lastTuple :: ?
 _lastTuple =
 (\r lastTuple x l -> (IF (< l 2) (CDR x) (lastTuple (CDR x) (- l 1))))
 
+; _foldr :: (a -> b -> b) -> b -> [a] -> b
 _foldr =
-(\r foldr f base xs ->
-    (IF (NIL xs) base (f (CAR xs) (foldr f base (CDR xs)))))
+(\ f base xs ->
+    (IF (NIL xs) base (f (CAR xs) (_foldr f base (CDR xs)))))
 
+; _map :: (a -> b) [a] -> [b]
 _map =
-(\r map f xs -> (IF (NIL xs) 0 (: (f (CAR xs)) (map f (CDR xs)))))
+(\ f xs -> (IF (NIL xs) 0 (: (f (CAR xs)) (_map f (CDR xs)))))
 
+; _filter :: (a -> Int) -> [a] -> [a]
+_filter =
+(\ p l -> (IF (NIL l) 0 (IF (p (CAR l))
+                            (: (CAR l) (_filter p (CDR l)))
+                            (_filter p (CDR l)))))
+
+; _elem :: Eq a => a -> [a] -> Int
 _elem =
 (\ x xs -> (_foldr (\ y v -> (_or (== x y) v)) 0 xs))
 
+; _mod :: Int -> Int -> Int
 _mod =
-(\r n m -> (- n (/ n m)))
+(\ n m -> (- n (* m (/ n m))))
 
+; _and :: Int -> Int -> Int
 _and =
 (\ a b -> (IF (== a 1) (IF (== b 1) 1 0) 1))
 
+; _or :: Int -> Int -> Int
 _or =
 (\ a b -> (IF (== a 0) (IF (== b 0) 0 1) 1))
 
+; _not :: Int -> Int
 _not =
 (\ a -> (IF (== a 0) 1 0))
-
-;_filter :: (a -> Int) -> [a] -> [a]
-_filter = (\ p l -> (if (nil l) 0 (if (p (car l)) (: (car l) (_filter p (cdr l))) (_filter p (cdr l))))) 
-
