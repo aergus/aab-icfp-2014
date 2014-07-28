cea552c233f5716467b7a22613394661db5b2bd3
Aras Ergus <alpha.ergus@gmail.com>
2014-07-28 10:11:51 +0200

    correct argument order @ DFS (now it works!)

--- Strategies/LambdaMen/lambdaDFSer.lisp
+++ Strategies/LambdaMen/lambdaDFSer.lisp
@@ -2,14 +2,14 @@
 
 -- seek it, go to it
 main =
-(: (_computeRoute (_matrixSearchR 4 (CAR WORLDSTATE) 0)
-                  (_myPos WORLDSTATE)
+(: (_computeRoute (_myPos WORLDSTATE)
+                  (_matrixSearchR 4 (CAR WORLDSTATE) 0)
                   (CAR WORLDSTATE)
    )
    (\ ai world ->
       (IF (NIL ai)
-          { route <- (_computeRoute (_matrixSearchR 3 (CAR world) 0)
-                                    (_myPos world)
+          { route <- (_computeRoute (_myPos world)
+                                    (_matrixSearchR 3 (CAR world) 0)
                                     (CAR world)
                      );
             (IF (NIL route) (: 0 0) (: (CDR route) (CAR route)))
